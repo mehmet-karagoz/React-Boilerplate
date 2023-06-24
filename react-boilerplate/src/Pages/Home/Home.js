0ad4e142528ba20660453257bb2c7fd2d186ac80
mehmet-karagoz <20190808026@ogr.akdeniz.edu.tr>
2023-06-24 16:32:32 +0300

    added counter hook

--- react-boilerplate/src/Pages/Home/Home.js
+++ react-boilerplate/src/Pages/Home/Home.js
@@ -1,14 +1,15 @@
-// Home.js
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUser } from '../../redux/slices/userSlice';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import useCustomHook from '../../hooks/useCustomHook';
 import './Home.css';
 
 const Home = () => {
   const dispatch = useDispatch();
   const user = useSelector(state => state.user);
+  const { count, increment } = useCustomHook();
 
   const handleLogin = () => {
     const user = { name: 'John Doe' };
@@ -23,6 +24,8 @@ const Home = () => {
         <p>This is the home page.</p>
         <button onClick={handleLogin}>Login</button>
         {user.name && <p>Welcome, {user.name}!</p>}
+        <p>Count: {count}</p>
+        <button onClick={increment}>Increment</button>
       </div>
       <Footer/>
     </>
